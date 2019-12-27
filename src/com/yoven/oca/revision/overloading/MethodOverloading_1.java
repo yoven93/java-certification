0dd613caa93eb767a376d5920cd4fac8881c7e5f
Yoven <krisnasamy.ayassamy@accenture.com>
2019-12-27 16:27:25 +0400

    first commit

---
+++ src/com/yoven/oca/revision/overloading/MethodOverloading_1.java
@@ -0,0 +1,9 @@
+package com.yoven.oca.revision.overloading;
+
+public class MethodOverloading_1 {
+	
+	public static void main(String[] args) {
+		
+		
+	}
+}
---
+++ src/com/yoven/oca/revision/typecasting/TypeCasting_1.java
@@ -0,0 +1,10 @@
+package com.yoven.oca.revision.typecasting;
+
+public class TypeCasting_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
